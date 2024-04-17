3a4d692018a01286b52d3f6f8e95f91dd435c229
Andrew LaNoue Personal <alanoue@catalyte.io>
2024-04-17 04:16:26 -0400

    Added temporary Projects page

--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import styles from "./index.module.scss";
 
-const index = () => {
-  return <div className={styles.project_container}>Test</div>;
+const ProjectPage = () => {
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.body_content}>
+        <h1 id={styles.title}> My Projects!</h1>
+        <p>
+          As of now, this portfolio is the only project I have. I am currently
+          working on more, such as a hospital patient management system as well
+          as a e-commerce application (both of which will be full-stack, with
+          the patient management system using Bootstrap and the e-commerce
+          application using .NET 8)
+        </p>
+        <p>
+          I thank you for your patience, and if you have any questions please
+          feel free to contact me! I would love to hear from you.
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default index;
+export default ProjectPage;
